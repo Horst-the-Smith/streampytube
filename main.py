318ead303055a39768d253501f1e526ba48517b3
Horst-the-Smith <tuichanpoint@gmail.com>
2025-03-03 22:03:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,21 +7,25 @@ st.title("Youtube Downloder")
 st.session_state['URL'] = ("")
 dt_now = datetime.datetime.now()
 
-url = st.text_input('input any youtube URL')
-
-if url:
-    buffer = BytesIO
-    st.session_state.URL = (url)
-    yt = YouTube(st.session_state.URL)
+@st.cache_data(show_spinner=False)
+def download_audio_to_buffer(url):
+    buffer = BytesIO()
+    yt = YouTube(url)
     video = yt.streams.get_highest_resolution()
     video.stream_to_buffer(buffer)
+    return buffer
 
-st.download_button(
-    label="Download!!",
-    data=buffer,
-    file_name=str(dt_now),
-    mine="video"
-)
-    
+def main():
+    url = st.text_input("Insert Youtube URL:")
+    if url:
+        with st.spinner("Downloading Audio Stream from Youtube..."):
+            buffer = download_audio_to_buffer(url)
+        st.subheader("Title")
+        st.download_button(
+            label="Download mp3",
+            data=buffer,
+            file_name=str(dt_now),
+            mime="video/mp3")
 
-st.write(st.session_state.URL)
+if __name__ == "__main__":
+    main()
